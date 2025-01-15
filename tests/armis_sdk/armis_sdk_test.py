527a9eef717738bcdf655abdd73990331076a061
Shai Lachmanovich <shai@armis.com>
2025-01-15 16:58:29 +0100

    Reorganise files (#9)

---
+++ tests/armis_sdk/armis_sdk_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from armis_sdk import ArmisSdk
+from armis_sdk.clients.sites_client import SitesClient
+from armis_sdk.core.armis_client import ArmisClient
+
+pytest_plugins = ["tests.plugins.setup_plugin"]
+
+
+@pytest.mark.usefixtures("setup_env_variables")
+def test_init_of_sdk():
+    armis_sdk = ArmisSdk()
+
+    assert isinstance(armis_sdk.client, ArmisClient)
+    assert isinstance(armis_sdk.sites, SitesClient)
